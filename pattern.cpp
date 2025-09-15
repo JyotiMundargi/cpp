9c2f38e3ecaaa9c75df54e516ed680addb0fdbc1
Jyoti Mundargi <163743624+JyotiMundargi@users.noreply.github.com>
2025-09-15 22:47:56 +0530

    Add files via upload

---
+++ pattern.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row;
+
+    cout<<"enter the size of row:5";
+    cin>>row;
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
